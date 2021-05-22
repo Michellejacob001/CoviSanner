9688dba726aefd92009a84a6c28d5ec3a865614a
Sanjana Prakash <68433048+Sanjana-11@users.noreply.github.com>
2021-05-22 08:36:53 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,120 @@
+import 'package:covi_scanner/scanner.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:covi_scanner/create_account.dart';
-import 'package:covi_scanner/loginui.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-///void main() => runApp(new MyApp());
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+import 'generate.dart';
+
+
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+      appBar: AppBar(
+      backgroundColor: Colors.red,
+      ),
+      ),
+  ),
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    
-      title: 'LoginUI',
+
+      //Given Title
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      //Given Theme Color
       theme: ThemeData(
-        accentColor: Color(0xff6A9347),
-       
+        backgroundColor: Color(0xFF10202B),
+      primarySwatch: Colors.blueGrey,
       ),
-     home: CreateAccount()
-     //MyHomePage(), 
+      //Declared first page of our app
+      home: HomePage(),
     );
   }
 }
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        body: Container(
+          width: 500,
+          height: 700,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+
+                decoration: BoxDecoration(
+
+                  border: Border.all(
+                    width: 5,
+                  ),
+                ),
+                child: Image.network("https://image.freepik.com/free-psd/hand-using-mobile-smart-phone-scan-qr-code_34168-2030.jpg"),
+                height: 250,
+              ),
+              //Display Image
+              // Image(
+              //   image: NetworkImage("https://image.freepik.com/free-psd/hand-using-mobile-smart-phone-scan-qr-code_34168-2030.jpg"),
+              //   height: 200,
+              //
+              // ),
+              SizedBox(height: 40),
+              //First Button
+
+              FlatButton(
+
+                color: Color(0xFF7D8A93),
+                padding: EdgeInsets.all(15),
+                onPressed: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ScanQR()));
+                },
+                child: Text("Scan QR Code",style: TextStyle(color: Colors.white),),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(color: Colors.indigo[900]),
+                ),
+              ),
+              SizedBox(height: 30),
+
+              //Second Button
+              FlatButton(
+                color: Color(0xFF7D8A93),
+                padding: EdgeInsets.all(15),
+                onPressed: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=>
+                      GenerateQR()));
+                },
+                child: Text("Generate QR Code", style: TextStyle(color: Colors.white),),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(color: Colors.indigo[900]),
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
+
+
+
 
